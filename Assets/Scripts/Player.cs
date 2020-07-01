6411e7d866f443c9318c8394a7d77c5f5b9efacf
mk1m <61814632+mk1m@users.noreply.github.com>
2020-06-30 17:47:12 -0700

    game updates

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //config params
+
+    private Rigidbody2D playerRB;
+
+    [Header("Player")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] public int health = 200;
+
+    [Header("VFX")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+
+    [Header("Sound")]
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.25f;
+
+    public Joystick joystick;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    float startTime;
+    bool TimerOn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+
+        //starting score timer
+        TimerOn = true;
+        startTime = Time.time;
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (TimerOn)
+        {
+            float currentTime = Time.time;
+            if (currentTime - startTime >= 0.5f)
+            {
+                FindObjectOfType<GameSession>().AddToScore(5);
+                startTime = currentTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+
+        //when player collides with a projectile, then the score stops
+        if (other.gameObject.tag == "Player")
+        {
+            TimerOn = false;
+        }
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<Level>().LoadEnd();
+        Destroy(gameObject);
+
+        //video effects for player death
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSoundVolume);
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        // joystick horizontal movement
+        playerRB.velocity = new Vector3(joystick.Horizontal * moveSpeed,
+            playerRB.velocity.y,
+            joystick.Vertical * moveSpeed);
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+    
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] int health = 200;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    { 
+        Destroy(gameObject);
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+}
